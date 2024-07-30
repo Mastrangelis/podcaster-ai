946687dffda8b135c46c9ea172a4ca5900198f46
Nikos Mastrangelis <mastragelis@outlook.com>
2024-07-30 15:33:31 +0300

    api: Created necessary file, openai and postacasts actions compatible with convex

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,7 +14,10 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as files from "../files.js";
 import type * as http from "../http.js";
+import type * as openai from "../openai.js";
+import type * as podcasts from "../podcasts.js";
 import type * as users from "../users.js";
 
 /**
@@ -26,7 +29,10 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  files: typeof files;
   http: typeof http;
+  openai: typeof openai;
+  podcasts: typeof podcasts;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
